453139a52f27fb249a2523a7b5ed4fb611e62c1e
ajay1931 <ajayprakash032@gmail.com>
2024-10-23 09:17:12 +0530

    files updated

--- script.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js').then(registration => {
-            console.log('ServiceWorker registered: ', registration);
-        }).catch(registrationError => {
-            console.log('ServiceWorker registration failed: ', registrationError);
-        });
-    });
-}
